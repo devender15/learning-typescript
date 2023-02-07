dba5d6f2a0772e7e3f079d430c02c6ecf483368c
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-02-07 19:01:16 +0530

    day 1

---
+++ sandbox.js
@@ -0,0 +1,4 @@
+var character = "Devender";
+console.log(character);
+var inputs = document.querySelectorAll("input");
+console.log(inputs);
---
+++ sandbox.ts
@@ -0,0 +1,6 @@
+const character = "Devender";
+
+console.log(character);
+
+const inputs = document.querySelectorAll("input");
+console.log(inputs);
