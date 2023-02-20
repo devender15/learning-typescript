e0b4f2a577acceae10499c68d37c4d999b92cea6
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-02-20 13:11:08 +0530

    learned about explicit types

--- sandbox.js
+++ sandbox.js
@@ -12,31 +12,68 @@
 // }
 // console.log(circ(5));
 // Array
-var names = ["Devender", "Kumar", "Singh"];
-// names = 23;
-// names = "Dev";
-names.push("Pal");
+// let names = ["Devender", "Kumar", "Singh"];
+// // names = 23;
+// // names = "Dev";
+// names.push("Pal");
 // names.push(4);
 // names[0] = 3.2;
-var numbers = [10, 20, 30, 40];
-numbers.push(25);
-// numbers.push("Devender");
-var mixed = ["Devender", 30, "Kumar", 40, true];
-mixed.push(50);
-mixed.push("Singh");
-mixed.push(false);
-// Objects
-var ninja = {
+// let numbers = [10, 20, 30, 40];
+// numbers.push(25);
+// // numbers.push("Devender");
+// let mixed = ["Devender", 30, "Kumar", 40, true];
+// mixed.push(50);
+// mixed.push("Singh");
+// mixed.push(false);
+// // Objects
+// let ninja = {
+//     name: "Devender",
+//     belt: "Black",
+//     age: 30,
+// };
+// ninja.age = 40;
+// // ninja.age = '40';
+// ninja.name = "Devender";
+// // ninja.skills = ["React", "Python", "django", "CSS", "JavaScript", "TypeScript"];
+// ninja = {
+//     name: "Devender",
+//     belt: "Black",
+//     age: 30,
+//     // skills: []
+// }
+// expicit types
+var character;
+var age;
+var isLoggedIn;
+// character = 5;
+character = "Dev";
+// age = "Devender";
+age = 30;
+// isLoggedIn = 1;
+isLoggedIn = false;
+// arrays
+var ninjas = [];
+// ninjas = [1,2,3];
+// ninjas = ["Devender", "Kumar", "Singh"];
+ninjas.push("Dev");
+// union types
+var mixed = [];
+mixed.push("Dev");
+mixed.push(19);
+mixed.push(true);
+// objects
+var ninjaOne;
+ninjaOne = {
     name: "Devender",
-    belt: "Black",
     age: 30
 };
-ninja.age = 40;
-// ninja.age = '40';
-ninja.name = "Devender";
-// ninja.skills = ["React", "Python", "django", "CSS", "JavaScript", "TypeScript"];
-ninja = {
+// ninjaOne.name = "Dev";
+// ninjaOne.age = 30;
+// ninjaOne = [];
+var ninjaTwo;
+ninjaTwo = {
     name: "Devender",
-    belt: "Black",
-    age: 30
+    age: 30,
+    beltColor: "Black"
 };
+ninjaTwo.name = "Jay";
--- sandbox.ts
+++ sandbox.ts
@@ -20,37 +20,89 @@
 
 
 // Array
-let names = ["Devender", "Kumar", "Singh"];
-// names = 23;
-// names = "Dev";
-names.push("Pal");
+// let names = ["Devender", "Kumar", "Singh"];
+// // names = 23;
+// // names = "Dev";
+// names.push("Pal");
 // names.push(4);
 // names[0] = 3.2;
 
-let numbers = [10, 20, 30, 40];
-numbers.push(25);
-// numbers.push("Devender");
+// let numbers = [10, 20, 30, 40];
+// numbers.push(25);
+// // numbers.push("Devender");
 
-let mixed = ["Devender", 30, "Kumar", 40, true];
-mixed.push(50);
-mixed.push("Singh");
-mixed.push(false);
+// let mixed = ["Devender", 30, "Kumar", 40, true];
+// mixed.push(50);
+// mixed.push("Singh");
+// mixed.push(false);
 
-// Objects
-let ninja = {
+// // Objects
+// let ninja = {
+//     name: "Devender",
+//     belt: "Black",
+//     age: 30,
+// };
+
+// ninja.age = 40;
+// // ninja.age = '40';
+// ninja.name = "Devender";
+// // ninja.skills = ["React", "Python", "django", "CSS", "JavaScript", "TypeScript"];
+
+// ninja = {
+//     name: "Devender",
+//     belt: "Black",
+//     age: 30,
+//     // skills: []
+// }
+
+
+
+// expicit types
+let character: string | number;
+let age: number;
+let isLoggedIn: boolean;
+
+// character = 5;
+character = "Dev";
+
+// age = "Devender";
+age = 30;
+
+// isLoggedIn = 1;
+isLoggedIn = false;
+
+// arrays
+let ninjas: string[] = [];
+// ninjas = [1,2,3];
+// ninjas = ["Devender", "Kumar", "Singh"];
+ninjas.push("Dev");
+
+// union types
+let mixed: (string | boolean | number)[] = [];
+mixed.push("Dev");
+mixed.push(19);
+mixed.push(true);
+
+// objects
+let ninjaOne: object;
+ninjaOne = {
     name: "Devender",
-    belt: "Black",
     age: 30,
-};
-
-ninja.age = 40;
-// ninja.age = '40';
-ninja.name = "Devender";
-// ninja.skills = ["React", "Python", "django", "CSS", "JavaScript", "TypeScript"];
-
-ninja = {
-    name: "Devender",
-    belt: "Black",
-    age: 30,
-    // skills: []
 }
+// ninjaOne.name = "Dev";
+// ninjaOne.age = 30;
+// ninjaOne = [];
+
+let ninjaTwo: {
+    name: string,
+    age: number,
+    beltColor: string,
+}
+
+ninjaTwo = {
+    name: "Devender",
+    age: 30,
+    beltColor: "Black",
+}
+
+ninjaTwo.name = "Jay";
